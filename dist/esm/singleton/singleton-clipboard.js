d5460c99fb73060b0b4776841fe1dd7ddb87efec
Delluna Builder <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-01 19:52:42 +0000

    build: update Delluna distribution

---
+++ dist/esm/singleton/singleton-clipboard.js
@@ -0,0 +1,4 @@
+export const name="singleton-clipboard";
+export const id="dl_6e6688875bbf423e9c23";
+export const url=new URL("../../icons/singleton/singleton-clipboard.svg?v=e117d77a4973157ebb6c62b12d833e97ffcec0103103ee82a7663b0fa3905f01",import.meta.url).href;
+export async function svg(){return fetch(url).then(r=>r.text())}
