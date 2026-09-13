b3ebe97c1d9a6f14d1b089feeaa47ff8f36b307e
Delluna Builder <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-13 10:40:02 +0000

    build: update Delluna generated artifacts [skip ci]

--- dist/esm/pencil.mjs
+++ dist/esm/pencil.mjs
@@ -1,6 +1,6 @@
 export const name="pencil";
 export const id="dl_e6327289cb864926a87c";
-export const url=new URL("../icons/pencil.svg?v=59c456fd1164519962c4828df6bc803e9659800c1ca478bac2ad477b220a79ec",import.meta.url).href;
+export const url=new URL("../icons/vkdico/pencil.svg?v=07efbedcf90fe3f6686c94677790739e80817c7b4d6d9087b23349155b1f69aa",import.meta.url).href;
 export async function svg(){return fetch(url).then(r=>{if(!r.ok)throw new Error(`Delluna icon fetch failed: HTTP ${r.status}`);return r.text()})}
 const icon={name,id,url,svg};
 export default icon;
