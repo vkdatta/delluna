665fccec016e1af8da43c87d34d85f3b7a460e0c
Delluna Builder <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-18 09:28:13 +0000

    build: update Delluna generated artifacts [skip ci]

---
+++ dist/esm/zone_person_idle-fill.mjs
@@ -0,0 +1,6 @@
+export const name="zone_person_idle-fill";
+export const id="dl_d56baaba87934f88bc2b";
+export const url=new URL("../icons/Z/zone_person_idle-fill.svg?v=cc3686284794bdc441b1e6540f898969170e0f6506cebb1da676ee03d2dd9c2a",import.meta.url).href;
+export async function svg(){return fetch(url).then(r=>{if(!r.ok)throw new Error(`Delluna icon fetch failed: HTTP ${r.status}`);return r.text()})}
+const icon={name,id,url,svg};
+export default icon;
---
+++ dist/esm/zoom_out-fill.mjs
@@ -0,0 +1,6 @@
+export const name="zoom_out-fill";
+export const id="dl_a5cad2dd2b9943ddbbeb";
+export const url=new URL("../icons/Z/zoom_out-fill.svg?v=08f5ec6abae5eb0ce3e9abced9903142f735abd54ca63bf05f5471de96e21864",import.meta.url).href;
+export async function svg(){return fetch(url).then(r=>{if(!r.ok)throw new Error(`Delluna icon fetch failed: HTTP ${r.status}`);return r.text()})}
+const icon={name,id,url,svg};
+export default icon;
